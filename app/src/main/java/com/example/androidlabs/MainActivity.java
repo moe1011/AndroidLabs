761a3012ad5da9745a08c5e9b9f4daae0a5a7450
moe1011 <77053716+moe1011@users.noreply.github.com>
2022-09-29 16:53:32 -0400

    Lab 3

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,14 +1,16 @@
 package com.example.androidlabs;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -17,40 +19,43 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main_linear);
-//        setContentView(R.layout.activity_main_grid);
-        setContentView(R.layout.activity_main_constraint);
+        setContentView(R.layout.activity_main);
 
-        //Get Edit Text Value
-        EditText editText = findViewById(R.id.editText);
 
-        //Press Me Button
-        final Button pressMeBtn = findViewById(R.id.pressMeBtn);
-        //Text View
-        TextView textView = findViewById(R.id.textView);
-        //Set Text View Value to the one stored in Edit Text
-        pressMeBtn.setOnClickListener( (click) -> {
-            textView.setText(editText.getText());
-            //Toast
-            String toastMsg = getResources().getString(R.string.toast_message);
-            Toast.makeText(MainActivity.this, toastMsg, Toast.LENGTH_LONG).show();
-        });
+        //Setting shared prefs
+        SharedPreferences prefs = getSharedPreferences("Lab3", Context.MODE_PRIVATE);
+        //Retrieving shared prefs data if it has been stored
+        EditText editText = findViewById(R.id.editTextTextPersonName);
+        editText.setText(prefs.getString("editText", ""));
 
-        CheckBox checkBox = findViewById(R.id.checkBox);
-        checkBox.setOnCheckedChangeListener( (cb, isChecked) -> {
-            String status = "";
-            String undoTxt = getResources().getString(R.string.snackbar_text_undo);
-            if(isChecked == true){
-                status = getResources().getString(R.string.snackbar_text_on);
-            } else{
-                status  = getResources().getString(R.string.snackbar_text_off);
-            }
-            Snackbar.make(cb, status, Snackbar.LENGTH_LONG)
-                    .setAction(undoTxt, click->cb.setChecked(!isChecked))
-                    .show();
-
+        Button nextButton = findViewById(R.id.nextButton);
+        Intent nextPage = new Intent(this, NameActivity.class);
+        nextButton.setOnClickListener(click -> {
+            nextPage.putExtra("name", editText.getText().toString());
+            startActivityForResult(nextPage, 0);
         });
 
 
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //Storing pref data
+        SharedPreferences prefs = getSharedPreferences("Lab3", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        EditText editText = findViewById(R.id.editTextTextPersonName);
+        editor.putString("editText", editText.getText().toString());
+        editor.commit();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == 1){
+            finish();
+        }
+    }
 }
+
