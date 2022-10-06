3bc37ed6168d50fcea5c3274c35ec53a10f53a53
moe1011 <77053716+moe1011@users.noreply.github.com>
2022-10-05 21:03:47 -0400

    Lab 4

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,61 +1,123 @@
 package com.example.androidlabs;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.TextView;
 
-
-import com.google.android.material.snackbar.Snackbar;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<TodoItem> elements = new ArrayList<>();
+    private MyListAdapter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ListView myList = findViewById(R.id.todoList);
+        myList.setAdapter( myAdapter = new MyListAdapter());
 
-        //Setting shared prefs
-        SharedPreferences prefs = getSharedPreferences("Lab3", Context.MODE_PRIVATE);
-        //Retrieving shared prefs data if it has been stored
+        Button addButton = findViewById(R.id.addBtn);
         EditText editText = findViewById(R.id.editTextTextPersonName);
-        editText.setText(prefs.getString("editText", ""));
+        Switch urgentSwitch = findViewById(R.id.urgentSwitch);
 
-        Button nextButton = findViewById(R.id.nextButton);
-        Intent nextPage = new Intent(this, NameActivity.class);
-        nextButton.setOnClickListener(click -> {
-            nextPage.putExtra("name", editText.getText().toString());
-            startActivityForResult(nextPage, 0);
+        addButton.setOnClickListener(click -> {
+            elements.add(new TodoItem(editText.getText().toString(), urgentSwitch.isChecked()));
+            editText.setText("");
+            myAdapter.notifyDataSetChanged();
+        });
+
+        myList.setOnItemLongClickListener((p, b, pos, id) -> {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setTitle(getResources().getString(R.string.alert_title))
+                    .setMessage(getResources().getString(R.string.alert_message)+ pos)
+                    .setPositiveButton("Yes", (click, arg) -> {
+                        elements.remove(pos);
+                        myAdapter.notifyDataSetChanged();
+                    })
+                    .setNegativeButton("No", (click, arg) ->{})
+                    .create().show();
+
+            return true;
         });
 
 
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        //Storing pref data
-        SharedPreferences prefs = getSharedPreferences("Lab3", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = prefs.edit();
-        EditText editText = findViewById(R.id.editTextTextPersonName);
-        editor.putString("editText", editText.getText().toString());
-        editor.commit();
+    private class TodoItem {
+        private String todo;
+        private boolean isUrgent;
 
-    }
+        public TodoItem(String todo, boolean isUrgent) {
+            this.todo = todo;
+            this.isUrgent = isUrgent;
+        }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode == 1){
-            finish();
+        public String getTodo() {
+            return todo;
+        }
+
+        public boolean isUrgent() {
+            return isUrgent;
         }
     }
-}
 
+    private class MyListAdapter extends BaseAdapter{
+        @Override
+        public int getCount() {
+            return elements.size();
+        }
+
+        @Override
+        public TodoItem getItem(int pos) {
+            return elements.get(pos);
+        }
+
+        @Override
+        public long getItemId(int pos) {
+            return pos;
+        }
+
+        @Override
+        public View getView(int pos, View view, ViewGroup parent) {
+            View newView = view;
+            LayoutInflater inflater = getLayoutInflater();
+            Switch urgentSwitch = findViewById(R.id.urgentSwitch);
+
+            //make a new row:
+            if(newView == null) {
+                newView = inflater.inflate(R.layout.todo_layout, parent, false);
+            }
+            //set what the text should be for this row:
+            TextView tView = newView.findViewById(R.id.todoText);
+            tView.setText( getItem(pos).getTodo());
+
+
+            if(getItem(pos).isUrgent()){
+                newView.setBackgroundColor(Color.RED);
+                tView.setTextColor(Color.WHITE);
+            } else {
+                newView.setBackgroundColor(Color.WHITE);
+                tView.setTextColor(Color.BLACK);
+            }
+
+            //return it to be put in the table
+            return newView;
+        }
+    }
+
+}
